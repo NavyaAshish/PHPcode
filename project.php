fb6c88f31c0bafb4d0f7c220052cf5ee0ed127a9
NavyaAshish <42655053+NavyaAshish@users.noreply.github.com>
2018-09-02 15:05:52 +0530

    Add files via upload

---
+++ project.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title>ENGINE SEARCH</title>
+<style>
+body { 
+        background-image: url("aaaaa.png"); }
+.inset-in{
+	width:190px;
+	border-style:inset;
+	float:left;
+	margin-right: 20px;
+	border-width:7px;border-color:black;background-color:rgba(255,255,255,0.4);color: black;text-align: center;padding: 10px;text-decoration: none;
+}
+
+.inset
+{border-style:inset;
+	
+	border-width:7px;border-color:black;background-color: rgba(255,255,255,0.3);color: black; font-size: 22px;text-align: center;padding: 10px;text-decoration: none;}
+/*.back{ color:yellow;text-align:center;background-color:teal;padding:10px;}*/
+/*.backa{ color:yellow;text-align:center;background-color:teal;margin:200px;width:200px;}*/
+/*.ps_fix{top:100px;right:100px;}*/
+/*.back{ color:yellow;text-align:center;background-color:teal;margin:10px;}*/
+</style>
+</head>
+<body>
+<p class="inset" style="margin:20px;" <img  width="25" height="35" src="smj.gif" border="0" align="left"/> TRAIN SEARCH ENGINE
+<br>INDIAN RAILWAYS</p>
+<br><br>
+<div style="margin:auto;width:70%;">
+<a class="inset-in"  href="spot.php"  target=_blank>Spot Your Train</a>
+<a class="inset-in"  href="live.php"  target=_blank>Live Stations</a>
+<a class="inset-in"  href="train.php"  target=_blank>Train Schedule</a>
+<br>
+<br><br>
+<br>
+<a class="inset-in"  href="stations.php" target=_blank>Train Between Stations</a>
+<a class="inset-in" href="cancelled.php"  target=_blank>	Cancalled Train</a>
+<a class="inset-in" href="rescheduled.php"  target=_blank>Reschedule Trains</a>
+<br><br><br>
+<br>
+<a class="inset-in" href="diverted.php"  target=_blank>Diverted Trains</a>
+<a class="inset-in" href="pnr.php"  target=_blank>PNR Status</a>
+<a class="inset-in" href="seat.php" target=_blank>Seat Availiability</a>
+<br><br><br><br>
+<!-- <a class="inset-in" href="ticket.php"  target=_blank>Ticket Booking</a> -->
+<br><br><br><br></div>
+<div style="width:100px;height:60px;text-align: center;font-family:sans-serif;margin-left: 940px; " ><i>Follow Us On</b><a href=https://www.facebook.com><img src="fbb.jpg" ></a>  <a href=https://www.twitter.com;><img src="twi.jpg" ></a></i></div>
+<div style="width:1200px;margin-right:0px;margin-left:0px;height:100px;background-color: black;color:black;">
+<footer style="color:yellow;text-align:center;line-height: 25px;font-family: Open Sans;"> |<a href="feedback.php" target=_blank >Feedback</a> | <a href="complaints.php">Complaints</a> | <br>Property Of indian Railways<br>
+Copyright @2017 Center for Railway Information System
+<br> all right reserved
+</body>
+</html>
